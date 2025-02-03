764f7c02bc76c94f3e0d2e4481cf034345368bfc
DavidAkerlind <david.akerlind@icloud.com>
2025-02-03 14:09:30 +0100

    Made validation form function and edited the html to work better for validation

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,55 @@
-const log = (msg) => console.log(msg);
+function validateForm() {
+    console.log("validateForm()");
+    let trainerName = document.querySelector("#nick");
+    let trainerAge = document.querySelector("#age");
+    let isTrainerGenderBoy = document.querySelector("#boy");
+    let isTrainerGenderGirl = document.querySelector("#girl");
 
-// I denna fil skriver ni all er kod
+    try {
+        if (trainerName.value.length < 5 || trainerName.value.length > 10) {
+            throw {
+                message: "Your nickname must be 5 to 10 letters of length.",
+                nodeRef: trainerName,
+            };
+        } else if (isNaN(trainerAge.value) || trainerAge.value === "") {
+            throw {
+                message: "You must enter a valid number as your age.",
+                nodeRef: trainerName,
+            };
+        } else if (trainerAge.value < 10 || trainerAge.value > 15) {
+            throw {
+                message:
+                    "You must be between 10 and 15 years old to play the game.",
+                nodeRef: trainerAge,
+            };
+        } else if (
+            !isTrainerGenderBoy.checked &&
+            !isTrainerGenderGirl.checked
+        ) {
+            throw {
+                message: "You must choose gender to play the game.",
+                nodeRef: trainerAge,
+            };
+        }
+        oGameData.trainerName = trainerName.value;
+        oGameData.trainerAge = trainerAge.value;
+        if (isTrainerGenderBoy.checked) {
+            oGameData.trainerGender = "Boy";
+        } else {
+            oGameData.trainerGender = "Girl";
+        }
+
+        return true;
+    } catch (error) {
+        document.querySelector("#errorMsg").textContent = error.message;
+        error.nodeRef.focus();
+
+        return false;
+    }
+}
+
+document.querySelector("#form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    document.querySelector("#errorMsg").textContent = "";
+    validateForm();
+});
